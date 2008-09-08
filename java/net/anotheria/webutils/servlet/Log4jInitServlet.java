feebc8517b39a7474cd99efc8568a25f160b8908
Oliver Toense <oliver.toense@getima.net>
2008-09-08 08:15:59 +0000

    added multiple possible locations of log4j.xml file

--- java/net/anotheria/webutils/servlet/Log4jInitServlet.java
+++ java/net/anotheria/webutils/servlet/Log4jInitServlet.java
@@ -1,27 +1,71 @@
 package net.anotheria.webutils.servlet;
 
+import java.io.File;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import net.anotheria.util.StringUtils;
+
 import org.apache.log4j.xml.DOMConfigurator;
 
 /** 
- * Taken from the log4j manual
- * Created on 14.07.2004 
+ * Usage in web.xml:
+ * <pre>
+ * <servlet>
+ *		<!-- das sollte mal bei gelegenheit in einen contextlistener rein! -->
+ *	    <servlet-name>log4j-init</servlet-name>
+ *	    <servlet-class>net.anotheria.webutils.servlet.Log4jInitServlet</servlet-class>
+ *	
+ *	    <init-param>
+ *	            <param-name>log4j-init-file</param-name>
+ *	            <param-value>WEB-INF/log4j.xml</param-value>
+ *	    </init-param>
+ *	
+ *	    <load-on-startup>1</load-on-startup>
+ *	</servlet>
+ * </pre>
+ * 
+ * The param-value may be a comma seperated list of files. The
+ * first file found will be used. If file-path starts with a '/'
+ * it will be treated as an absolut path otherwise it is relative
+ * to the webapp's docPath
+ *  
  */
 public class Log4jInitServlet extends HttpServlet {
 
-  public void init(ServletConfig config) throws ServletException {
-  	System.out.println("Initializing Log4jInit-Servlet.");
-  	String prefix =  config.getServletContext().getRealPath("/");
-  	String file = config.getInitParameter("log4j-init-file");
-  	// if the log4j-init-file is not set, then no point in trying
+	public static final String INIT_PARAM = "log4j-init-file";
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Initializing Log4jInit-Servlet.");
+		
+		String prefix =  config.getServletContext().getRealPath("/");
+		String log4jInitFileString = config.getInitParameter(INIT_PARAM);
+		String file = findFile(prefix, log4jInitFileString);
+		
+		// if the log4j-init-file is not set, then no point in trying
 		if(file != null) {
+			System.out.println("Log4jInit-Servlet: using file " + file);
 			DOMConfigurator.configureAndWatch(prefix+file);
 		} 
 		else 
 			throw new ServletException("Could not access log4j-init-file at "+file);
-  }
+	}
+	
+	private String findFile(String prefix, String log4jInitFileString) {
+		String[] files = StringUtils.tokenize(log4jInitFileString, ',');
+		for(int i=0; i<files.length; i++) {
+			String fileName = files[i];
+			if(!fileName.startsWith("/")) {
+				fileName = prefix + fileName;
+			}
+			File file = new File(fileName);
+			if(file.exists()) {
+				return fileName;
+			}
+		}
+		return null;
+	}
 
 }
